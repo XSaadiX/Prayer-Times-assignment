6582e512deaa95f51a5c9ee72fd2b807e804eb86
Your Name <SSHAMALLAHK@GMAIL.COM>
2025-09-19 12:24:04 +0300

    Add utility functions for Prayer Times App

---
+++ utils.js
@@ -0,0 +1,23 @@
+// utils.js - Utility functions for Prayer Times App
+
+// TODO: Format time to HH:MM:SS
+export function formatTime(dateObj) {
+  // Example: convert Date object to HH:MM:SS string
+}
+
+// TODO: Calculate next prayer time
+export function getNextPrayer(prayerTimes) {
+  // prayerTimes = { Fajr: "05:00", Dhuhr: "12:30", ... }
+  // TODO: Return next prayer name + time
+}
+
+// TODO: Countdown logic for next prayer
+export function startCountdown(nextPrayerTime, onTick, onComplete) {
+  // Call onTick(remainingTime) every second
+  // Call onComplete() when countdown ends
+}
+
+// TODO: Save & load last selections from localStorage
+export function saveSelections(selections) {}
+export function loadSelections() {}
+export function clearSelections() {}
