44ba3ff318c7d458a40014ff954a397dfaacafcb
Your Name <SSHAMALLAHK@GMAIL.COM>
2025-09-19 12:24:28 +0300

    Add main app logic to initialize Prayer Times application

---
+++ app.js
@@ -0,0 +1,15 @@
+import { fetchCountriesByContinent, fetchCitiesByCountry, fetchPrayerTimes } from './api.js';
+import { formatTime, getNextPrayer, startCountdown, saveSelections, loadSelections, clearSelections } from './utils.js';
+
+// Main app logic
+function init() {
+  console.log("App initialized");
+
+  // TODO: Load last selections from localStorage
+  // TODO: Setup event listeners for continent, country, city, calculation method
+  // TODO: Fetch data and render prayer table
+  // TODO: Start countdown for next prayer
+  // TODO: Reset button clears everything
+}
+
+init();
