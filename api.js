d09540f96c6c8b5f7283827c1d57ab8b1624b2e5
nora <noraashqar244@gmail.com>
2025-09-20 02:31:39 +0300

    Add API calls for countries, cities, and prayer times

--- api.js
+++ api.js
@@ -1,15 +1,42 @@
 // api.js - Handles all API calls for the Prayer Times App
 
-// TODO: Fetch countries by continent from REST Countries API
 export async function fetchCountriesByContinent(continent) {
-  // Example endpoint: https://restcountries.com/v3.1/region/{continent}
-  // TODO: Return array of country names
+  try{
+    const res=await fetch(`https://restcountries.com/v3.1/region/${continent}`);
+    if(!res.ok) 
+      throw new Error('Failed to fetch countries');
+
+    const data =await res.json();
+    return data.map(c =>c.name.common);
+  }catch (error){
+    console.error(error);
+    return [];
+  }
 }
 
-// TODO: Fetch cities by country from CountriesNow API 
+const cityCache =new Map();
 export async function fetchCitiesByCountry(country) {
-  // Example endpoint: https://countriesnow.space/api/v0.1/countries/cities
-  // TODO: Implement caching to avoid repeated calls
+  if(cityCache.has(country)){
+    return cityCache.get(country);
+  }
+  try {
+    const res=await fetch('https://countriesnow.space/api/v0.1/countries/cities',{
+    method:'POST',
+    body:JSON.stringify({country}),
+    headers:{
+      'Content-Type':'application/json'
+    },
+    })
+    if(!res.ok)
+      throw new Error('Failed to fetch cities');
+    const data =await res.json();
+    const cities =data.data || [];
+    cityCache.set(country,cities);
+    return cities;
+  } catch (error) {
+     console.error(error);
+    return [];
+  }
 }
 
 // TODO: Fetch prayer times from Aladhan API
